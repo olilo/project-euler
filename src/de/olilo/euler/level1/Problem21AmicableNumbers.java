27762a91ea73c91ef05bba25c7d09cc292332f18
Oliver Lorenz <oliver-lorenz@gmx.de>
2013-10-30 08:45:16 +0100

    removed unused imports

--- src/de/olilo/euler/level1/Problem21AmicableNumbers.java
+++ src/de/olilo/euler/level1/Problem21AmicableNumbers.java
@@ -3,9 +3,7 @@ package de.olilo.euler.level1;
 import de.olilo.util.Divisors;
 
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
-import java.util.Set;
 
 public class Problem21AmicableNumbers {
 
