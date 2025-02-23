405c2711627e1b1504a072d32c6c0d2922836153
Oliver Lorenz <lorenz@cosmocode.de>
2025-02-23 16:08:28 +0100

    Make problem 10 implement the Problem interface

--- src/de/olilo/euler/level1/Problem10SumOfPrimes.java
+++ src/de/olilo/euler/level1/Problem10SumOfPrimes.java
@@ -1,10 +1,27 @@
 package de.olilo.euler.level1;
 
+import de.olilo.euler.Problem;
+import de.olilo.euler.Runner;
 import de.olilo.util.PrimesIterable;
 
-class Problem10SumOfPrimes {
+public class Problem10SumOfPrimes implements Problem {
 
-    public long sumOfPrimesUpTo(int limit) {
+    @Override
+    public String getMessage() {
+        return "The sum of all primes up to 2.000.000 is: ";
+    }
+
+    @Override
+    public int getProblemNumber() {
+        return 10;
+    }
+
+    @Override
+    public Number runProblem(Runner runner) {
+        return sumOfPrimesUpTo(2_000_000);
+    }
+
+    protected long sumOfPrimesUpTo(int limit) {
         long sum = 0;
         for (int prime : PrimesIterable.INSTANCE) {
             if (prime <= limit) {
@@ -15,5 +32,4 @@ class Problem10SumOfPrimes {
         }
         return sum;
     }
-
 }
