182d621aef101d2688667f1777d6d727cba8a004
Oliver Lorenz <lorenz@cosmocode.de>
2025-02-24 10:09:12 +0100

    Make problem 3 implement Problem interface

--- src/de/olilo/euler/level1/Problem3LargestPrime.java
+++ src/de/olilo/euler/level1/Problem3LargestPrime.java
@@ -1,10 +1,29 @@
 package de.olilo.euler.level1;
 
+import de.olilo.euler.Problem;
+import de.olilo.euler.Runner;
 import de.olilo.util.PrimeFactors;
 
-class Problem3LargestPrime {
+import java.io.IOException;
 
-    public long getLargestPrimeOf(long number) {
+public class Problem3LargestPrime implements Problem {
+
+    @Override
+    public String getMessage() {
+        return "The largest prime factor of 600.851.475.143 is: ";
+    }
+
+    @Override
+    public int getProblemNumber() {
+        return 3;
+    }
+
+    @Override
+    public Number runProblem(Runner runner) throws IOException {
+        return getLargestPrimeOf(600_851_475_143L);
+    }
+
+    protected long getLargestPrimeOf(long number) {
         // the prime factors of the number
         int[] primeFactors = PrimeFactors.of(number);
         return primeFactors[primeFactors.length - 1];
